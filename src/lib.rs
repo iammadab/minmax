c2f2dabd2f5d90ebf99235e090fbf5ab4ffd6663
Wisdom Ogwu <iammadab@gmail.com>
2023-04-29 02:32:07 +0100

    implement game trait for tic tac toe

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use crate::Player::{O, X};
+
 trait Action {}
 
 trait Game<A: Action> {
@@ -6,15 +8,26 @@ trait Game<A: Action> {
     /// Returns all possible actions from the current game state
     fn get_actions(&self) -> Vec<A>;
     /// Applies an action to the current state, returns a new state
-    fn apply_action(&self, action: A) -> Self;
+    fn apply_action(&self, action: A) -> Result<Self, String>
+    where
+        Self: Sized;
     /// Get the value of the current state, must be terminal
-    fn value(&self) -> u8;
+    fn value(&self) -> i8;
 }
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 enum Player {
     X,
     O,
+}
+
+impl Player {
+    fn invert(&self) -> Self {
+        match self {
+            X => O,
+            O => X,
+        }
+    }
 }
 
 struct TicTacToe {
@@ -87,29 +100,73 @@ impl Game<TicTacToeAction> for TicTacToe {
     }
 
     fn get_actions(&self) -> Vec<TicTacToeAction> {
-        todo!()
+        if self.is_terminal() {
+            // cannot perform further actions once we have reached the terminal state
+            return Vec::new();
+        }
+
+        self.board
+            .iter()
+            .enumerate()
+            .filter_map(|(i, player_move)| match player_move {
+                None => Some(i as u8),
+                _ => None,
+            })
+            .collect()
     }
 
-    fn apply_action(&self, action: TicTacToeAction) -> Self {
-        todo!()
+    // TODO: make more efficient
+    //  there is no need to create a new struct for every state transition
+    //  should refactor so we only duplicate data and struct just contains
+    //  data transformation functions
+    fn apply_action(&self, action: TicTacToeAction) -> Result<Self, String> {
+        if self.is_terminal() {
+            return Err(String::from("game already reached terminal state"));
+        }
+
+        if action < 0 || action > 8 {
+            return Err(String::from(
+                "action can only be a number between 0 and 8 inclusive",
+            ));
+        }
+
+        if self.board[action as usize] != None {
+            return Err(String::from("move already made at given position"));
+        }
+
+        let mut new_board_state = self.board;
+        new_board_state[action as usize] = Some(self.player);
+
+        Ok(TicTacToe::new_from_state(
+            new_board_state,
+            self.player.invert(),
+        ))
     }
 
-    fn value(&self) -> u8 {
-        todo!()
+    fn value(&self) -> i8 {
+        // assumes that the current state is terminal
+        let winner = self.get_winner().unwrap();
+        match winner {
+            Some(Player::X) => 1,
+            Some(Player::O) => -1,
+            None => 0,
+        }
     }
 }
 
+// TODO: implement to u8
 type TicTacToeAction = u8;
 impl Action for TicTacToeAction {}
 
 #[cfg(test)]
 mod tests {
     use crate::Player::{O, X};
-    use crate::{Player, TicTacToe};
+    use crate::{Game, Player, TicTacToe};
 
     #[test]
     fn test_get_winner() {
         let game = TicTacToe::new();
+        assert_eq!(game.is_terminal(), false);
         assert_eq!(game.get_winner().is_err(), true);
 
         // Terminal state with x winning
@@ -127,6 +184,8 @@ mod tests {
             ],
             Player::O,
         );
+        assert_eq!(game.is_terminal(), true);
+        assert_eq!(game.value(), 1);
         let winner = game
             .get_winner()
             .expect("should be terminal")
@@ -148,6 +207,8 @@ mod tests {
             ],
             Player::X,
         );
+        assert_eq!(game.is_terminal(), true);
+        assert_eq!(game.value(), -1);
         let winner = game
             .get_winner()
             .expect("should be terminal")
@@ -169,7 +230,124 @@ mod tests {
             ],
             Player::X,
         );
+        assert_eq!(game.is_terminal(), true);
+        assert_eq!(game.value(), 0);
         let result = game.get_winner().expect("should be terminal");
         assert_eq!(result.is_none(), true);
+    }
+
+    #[test]
+    fn test_get_actions() {
+        // from empty board, all actions should be possible
+        let game = TicTacToe::new();
+        let actions = game.get_actions();
+        assert_eq!(actions.len(), 9);
+        assert_eq!(actions, [0, 1, 2, 3, 4, 5, 6, 7, 8]);
+
+        // non terminal and non draw
+        let game = TicTacToe::new_from_state(
+            [
+                Some(X),
+                Some(X),
+                None,
+                Some(O),
+                Some(O),
+                None,
+                None,
+                None,
+                None,
+            ],
+            Player::X,
+        );
+        let actions = game.get_actions();
+        assert_eq!(actions.len(), 5);
+        assert_eq!(actions, [2, 5, 6, 7, 8]);
+
+        // action set should be empty if terminal state
+        let game = TicTacToe::new_from_state(
+            [
+                Some(X),
+                Some(X),
+                Some(O),
+                Some(X),
+                Some(O),
+                None,
+                Some(O),
+                None,
+                None,
+            ],
+            Player::X,
+        );
+        let actions = game.get_actions();
+        assert_eq!(actions.len(), 0);
+    }
+
+    #[test]
+    fn test_apply_action() {
+        let game = TicTacToe::new();
+        let game = game.apply_action(0).unwrap();
+        assert_eq!(
+            game.board,
+            [Some(X), None, None, None, None, None, None, None, None]
+        );
+        let game = game.apply_action(3).unwrap();
+        assert_eq!(
+            game.board,
+            [Some(X), None, None, Some(O), None, None, None, None, None]
+        );
+        let game = game.apply_action(1).unwrap();
+        assert_eq!(
+            game.board,
+            [
+                Some(X),
+                Some(X),
+                None,
+                Some(O),
+                None,
+                None,
+                None,
+                None,
+                None
+            ]
+        );
+        let game = game.apply_action(4).unwrap();
+        assert_eq!(
+            game.board,
+            [
+                Some(X),
+                Some(X),
+                None,
+                Some(O),
+                Some(O),
+                None,
+                None,
+                None,
+                None
+            ]
+        );
+
+        // try to play a repeated move
+        assert_eq!(game.apply_action(4).is_err(), true);
+
+        // finish the game
+        let game = game.apply_action(2).unwrap();
+        assert_eq!(
+            game.board,
+            [
+                Some(X),
+                Some(X),
+                Some(X),
+                Some(O),
+                Some(O),
+                None,
+                None,
+                None,
+                None
+            ]
+        );
+        assert_eq!(game.is_terminal(), true);
+
+        // try to play after terminal
+        assert_eq!(game.apply_action(8).is_err(), true);
     }
 }
